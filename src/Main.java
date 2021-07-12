49f31a93135787541cd828c4761e9ddce0452793
Kim Myung Soo <msbmkim@wdfall.com>
2021-07-12 17:40:11 +0900

    base source push

--- src/Main.java
+++ src/Main.java
@@ -7,47 +7,11 @@ public class Main {
 	static Scanner sc = new Scanner(System.in);
 	static StringTokenizer st;
 	
-	static int N, M;
-	static int[] arr;
-	
 	public static void main(String[] args) throws Exception {
 		br = new BufferedReader(new FileReader(Main.class.getResource("").getPath() + "input"));
 		sc = new Scanner(new File(Main.class.getResource("").getPath() + "input"));
 		
-		N = Integer.parseInt(br.readLine());
-		arr = new int[N+1];
-		st = new StringTokenizer(br.readLine());
-		for(int i=1; i<=N; ++i) arr[i] = Integer.parseInt(st.nextToken());
 		
-		M = Integer.parseInt(br.readLine());
-		for(int query=0; query<M; ++query){
-			st = new StringTokenizer(br.readLine());
-			int sex = Integer.parseInt(st.nextToken());
-			int idx = Integer.parseInt(st.nextToken());
-			
-			if(sex == 1){
-				for(int i=idx; i<=N; i+=idx){
-					arr[i] ^= 1;
-				}
-			}else{
-				arr[idx] ^= 1;
-				for(int l=idx-1, r=idx+1; l>=1&&r<=N; --l,++r){
-					if(arr[l] == arr[r]){
-						arr[l] ^= 1;
-						arr[r] ^= 1;
-					}else{
-						break;
-					}
-				}
-			}
-		}
-		
-		StringBuilder sb = new StringBuilder();
-		for(int i=1; i<=N; ++i) {
-			sb.append(arr[i] + " ");
-			if(i%20 == 0) sb.append("\n");
-		}
-		System.out.println(sb.toString());
 		
 		br.close();
 		bw.close();
--- src/Main2.java
+++ src/Main2.java
@@ -2,16 +2,15 @@ import java.util.*;
 import java.io.*;
 
 class Main2 {
-	public static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-	public static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-	public static Scanner sc = new Scanner(System.in);
-	public static StringTokenizer st;
-	
+	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+	static Scanner sc = new Scanner(System.in);
+	static StringTokenizer st;
 	
 	public static void main(String[] args) throws Exception {
-//		br = new BufferedReader(new FileReader(new File(new FileReader(Main2.class.getResource("").getPath() + "input")));
-
-		Solution sol = new Solution();
+		br = new BufferedReader(new FileReader(Main2.class.getResource("").getPath() + "input"));
+		
+		
 		
 		br.close();
 		bw.close();
